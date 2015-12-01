e35756a74dfa589488614d52e508ce793b68ce0f
Rudy Reeves <rudyreeves@lclark.edu>
2015-12-01 15:47:45 -0500

    Added #NOQA to fix long URL strings

--- entertainment_center.py
+++ entertainment_center.py
@@ -4,13 +4,13 @@ import media
 # Create a Movie instance for toy_story, from the media module
 toy_story = media.Movie("Toy Story", 1995,
             "A story of a boy and his toys that come to life",
-            "https://upload.wikimedia.org/wikipedia/en/thumb/1/13/Toy_Story.jpg/220px-Toy_Story.jpg",
+            "https://upload.wikimedia.org/wikipedia/en/thumb/1/13/Toy_Story.jpg/220px-Toy_Story.jpg", # NOQA
             "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
 # Create another Movie instance for the_matrix
 the_matrix = media.Movie("The Matrix", 1999,
             "Neo discovers he is living in a computer simulation",
-            "https://upload.wikimedia.org/wikipedia/en/thumb/c/c1/The_Matrix_Poster.jpg/220px-The_Matrix_Poster.jpg",
+            "https://upload.wikimedia.org/wikipedia/en/thumb/c/c1/The_Matrix_Poster.jpg/220px-The_Matrix_Poster.jpg", # NOQA
             "https://www.youtube.com/watch?v=vKQi3bBA1y8")
 
 # Create a list of all our movies
